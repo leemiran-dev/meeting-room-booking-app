219dee93eecc8d807ee1ed8195e048dbca1401d1
leemiran-dev <leemiran.0729@gmail.com>
2026-03-19 15:46:45 +0900

    feat: 회의실 목록 조회 API 및 DB 연동 구현

--- src/main/java/com/example/meetingroom/MeetingroomApplication.java
+++ src/main/java/com/example/meetingroom/MeetingroomApplication.java
@@ -2,10 +2,9 @@ package com.example.meetingroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MeetingroomApplication {
 
     public static void main(String[] args) {
